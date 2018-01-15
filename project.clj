7edeb83cb091338073b616173aa73c8d6793dc77
ken restivo <ken@restivo.org>
2018-01-14 23:12:33 -0800

    Update version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject matrix-client-server-api "r0.3.0"
   :description "Client library of matrix-client-server-api"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.6.0"]
                  [cheshire "5.5.0"]])
