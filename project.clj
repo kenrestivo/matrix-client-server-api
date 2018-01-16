9b3cc0d15227eb3d110d667339fe50305a6e7cfa
ken restivo <ken@restivo.org>
2018-01-15 20:39:10 -0800

    Might as well own it

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matrix-client-server-api "r0.3.0"
+(defproject org.restivo/matrix-client-server-api "r0.3.0"
   :description "Client library of matrix-client-server-api"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.7.0"]
