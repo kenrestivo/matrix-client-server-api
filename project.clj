19bdb83f0550d288e3d33c23159602fc3c1b71c8
ken restivo <ken@restivo.org>
2018-01-15 20:37:31 -0800

    From swagger

--- project.clj
+++ project.clj
@@ -2,6 +2,4 @@
   :description "Client library of matrix-client-server-api"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.7.0"]
-                 [utilza "0.9.6"]
-                 [com.taoensso/timbre "4.10.0"]
                  [cheshire "5.8.0"]])
