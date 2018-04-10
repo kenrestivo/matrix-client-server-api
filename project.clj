40fdc33a1632f056eaf1b21c56baa6c362f5413f
ken restivo <ken@restivo.org>
2018-04-09 21:24:12 -0700

    Add uberdoc

--- project.clj
+++ project.clj
@@ -2,4 +2,8 @@
   :description "Client library of matrix-client-server-api"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.7.0"]
-                 [cheshire "5.8.0"]])
+                 [cheshire "5.8.0"]]
+  :codox {:output-path "docs"}
+  :profiles {:dev {:plugins [[lein-codox "0.10.3"]
+                             [lein-marginalia "0.9.1"]]}}
+  )
